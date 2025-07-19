585b7e0f8ac567c445f2774c66ba0774356cdce8
andreapaccagnella94 <andreapacca94@live.it>
2025-07-19 09:19:35 +0200

    add done the first script

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+//provo a fare una arrow function che è più snella
 
+const arrowGreeting = userName => `Ciao ${userName}`
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(arrowGreeting(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
